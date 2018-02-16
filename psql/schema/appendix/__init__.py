a273ed35c3ce657aa9116bc150f44c7c427b84a0
Charlie <ctwinnard@gmail.com>
2018-02-15 20:50:03 -0700

    updated db seed, added appendix module to psql

---
+++ psql/schema/appendix/__init__.py
@@ -0,0 +1,1 @@
+from .light_reference import LightReference
---
+++ psql/schema/appendix/hardware.py
@@ -0,0 +1,12 @@
+rom growflask import db
+
+from psql.schema.master import User
+
+
+class Lighting(db.Model):
+    """Represents a plant"""
+
+    __table_args__ = {'schema': 'appendix'}
+    __tablename__ = 'lighting'
+
+    id = db.Column(db.Integer, primary_key=True)

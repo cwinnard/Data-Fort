33b025d1cc834ebb9d1fcf122040b946e53898e3
Charlie <ctwinnard@gmail.com>
2018-02-18 09:49:45 -0700

    Create new user functionality

--- growflask/login/controller.py
+++ growflask/login/controller.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, redirect, render_template, request, url_for
 from flask_login import current_user
 
+from growflask import db
 from .login_facilitator import LoginFacilitator
 from psql.schema.master import User
 
@@ -15,4 +16,25 @@ def log_user_in():
     userName = request.args.get('username')
     user = User.query.filter_by(user_name=userName).first()
     LoginFacilitator().log_user_in(user)
+    return redirect(url_for('dashboard.dashboard'))
+
+@loginBP.route('/create-user')
+def create_user():
+    user_name = request.args.get('username')
+    name_first = request.args.get('firstname')
+    name_last = request.args.get('lastname')
+    email = request.args.get('email')
+    password = request.args.get('password')
+
+    newUser = User()
+    newUser.user_name = user_name
+    newUser.name_first = name_first
+    newUser.name_last = name_last
+    newUser.email = email
+    newUser.password = password
+
+    db.session.add(newUser)
+    db.session.commit()
+
+    LoginFacilitator().log_user_in(newUser)
     return redirect(url_for('dashboard.dashboard'))

4f99b023946183dd59b4cbd0d7aba457c5c6b84c
Charlie <ctwinnard@gmail.com>
2018-02-18 09:40:49 -0700

    some cleanup of app init

--- growflask/__init__.py
+++ growflask/__init__.py
@@ -1,26 +1,26 @@
-from flask import Flask
+from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import login_manager
 
 import config
 
+# Create clask app
 app = Flask(__name__)
-app.secret_key = 'TOP SECRET KEY'
+app.secret_key = 'STAND42.334526BY-83.045908'
 
+#Configure db connection
 app.config['SQLALCHEMY_DATABASE_URI'] = config.SQLALCHEMY_DATABASE_URI
 db = SQLAlchemy(app)
 
-#Need work on login
-
+#Configure login
 loginManager = login_manager.LoginManager()
-
 from psql.schema.master import User
 @loginManager.user_loader
 def load_user(user_id):
     return User.query.get(user_id)
-
 loginManager.init_app(app)
 
+#Configure blueprints
 from growflask.dashboard.controller import dashboardBP
 app.register_blueprint(dashboardBP)
 from growflask.login.controller import loginBP
@@ -32,4 +32,4 @@ app.register_blueprint(plantBP)
 
 @app.route('/')
 def index():
-	return 'hello application'
+	return redirect(url_for('login.welcome'))

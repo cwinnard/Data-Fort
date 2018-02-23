d0ce3450bbe46898b602efccac78d2ac1b43cc62
Charlie <ctwinnard@gmail.com>
2018-02-22 21:41:58 -0700

    id is not an integer in junction table

--- psql/schema/toolshed/toolshed_tool.py
+++ psql/schema/toolshed/toolshed_tool.py
@@ -9,4 +9,4 @@ class ToolshedTool(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     id_tool = db.Column(db.Integer, db.ForeignKey('toolshed.tool.id'), primary_key=True)
-    id_toolshed = db.Column(db.String(128), db.ForeignKey('toolshed.toolshed.id'), primary_key=True)
+    id_toolshed = db.Column(db.Integer, db.ForeignKey('toolshed.toolshed.id'), primary_key=True)

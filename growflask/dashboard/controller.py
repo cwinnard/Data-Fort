40b8ea677e75306a244ce8d109c73b40d14a450c
Charlie <ctwinnard@gmail.com>
2018-03-01 19:32:52 -0700

    Moved add tool to toolshed module

--- growflask/dashboard/controller.py
+++ growflask/dashboard/controller.py
@@ -4,8 +4,7 @@ from flask_login import current_user
 
 from growflask import db
 from psql.schema.growing import Plant
-from psql.schema.notebook import Reading, ReadType
-from psql.schema.toolshed import Tool, Toolshed
+from psql.schema.toolshed import Toolshed
 
 dashboardBP = Blueprint('dashboard', __name__, template_folder='templates', url_prefix='/dashboard')
 
@@ -35,21 +34,3 @@ def toolsheds_json():
     toolsheds = Toolshed.query.all()
     #toolsheds = Toolshed.query.filter_by(id_user=current_user.id).all()
     return jsonify(toolsheds=[toolshed.serialize() for toolshed in toolsheds])
-
-@dashboardBP.route('/<int:userId>/toolshed/<int:toolshedId>/add-tool')
-def toolshed_add_tool(userId, toolshedId):
-    name = request.args.get('name')
-    read_type_id = request.args.get('read_type')
-
-    toolshed = Toolshed.query.get(toolshedId)
-    read_type = ReadType.query.get(read_type_id)
-
-    tool = Tool()
-    tool.name = name
-    tool.read_type = read_type
-    tool.toolsheds_in += [toolshed]
-
-    db.session.add(tool)
-    db.session.commit()
-
-    return redirect(url_for('dashboard.toolshed')) 
--- growflask/toolshed/controller.py
+++ growflask/toolshed/controller.py
@@ -2,7 +2,8 @@ from flask import Blueprint, jsonify, redirect, render_template, request, url_fo
 from flask_login import current_user
 
 from growflask import db
-from psql.schema.toolshed import Toolshed
+from psql.schema.notebook import ReadType
+from psql.schema.toolshed import Tool, Toolshed
 
 toolshedBP = Blueprint('toolshed', __name__, template_folder='templates', url_prefix='/toolshed')
 
@@ -10,3 +11,21 @@ toolshedBP = Blueprint('toolshed', __name__, template_folder='templates', url_pr
 def toolshed(toolshedId):
     toolshed = Toolshed.query.get(toolshedId)
     return render_template('toolshed.html', toolshed=toolshed)
+
+@toolshedBP.route('/<int:toolshedId>/add-tool')
+def add_tool(toolshedId):
+    name = request.args.get('name')
+    read_type_id = request.args.get('read_type')
+
+    toolshed = Toolshed.query.get(toolshedId)
+    read_type = ReadType.query.get(read_type_id)
+
+    tool = Tool()
+    tool.name = name
+    tool.read_type = read_type
+    tool.toolsheds_in += [toolshed]
+
+    db.session.add(tool)
+    db.session.commit()
+
+    return redirect(url_for('toolshed.toolshed', toolshedId=toolshedId)) 

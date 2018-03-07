3dc3f5a787b2fa6eccf726311236812be8481144
Charlie <ctwinnard@gmail.com>
2018-03-06 19:30:51 -0700

    Had the foreign key definition messed up

--- psql/schema/growing/plant_photo.py
+++ psql/schema/growing/plant_photo.py
@@ -14,7 +14,7 @@ class PlantPhoto(db.Model):
     __tablename__ = 'plant_photo'
 
     id = db.Column(db.Integer, primary_key=True)
-    id_plant = db.Column(db.Integer, db.ForeignKey('psql.schema.growing.plant.id'), nullable=False)
+    id_plant = db.Column(db.Integer, db.ForeignKey('growing.plant.id'), nullable=False)
     ts_uploaded = db.Column(db.DateTime(), nullable=False)
     notes = db.Column(db.String(128))
     ts_deleted = db.Column(db.DateTime())
